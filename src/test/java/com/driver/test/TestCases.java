7bcc938171acc04036a892549d1b05ecfbfb9e08
Manjunath M Goni <manjugoni007@gmail.com>
2025-05-24 18:39:28 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -3,6 +3,35 @@ package com.driver.test;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
-public class TestCases {
+
+
+
+
+public class Main{
+
+  public static void main(String[] args){
     
+    B obj  = new B();
+    System.out.print(obj.meth());
+    
+  }
 }
+  
+  
+
+class A {
+  public String meth(){
+    
+    return "Invoking method from class A";
+  }
+}
+
+
+class B  extends A{
+  
+  public String meth(){
+    
+    return "Method is overridden in Extended class B";
+  }
+}
+
